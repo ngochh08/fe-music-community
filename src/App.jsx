9918b4eecf4b022910a3546fb45fff8e27994b81
ngochh <ngochh08@gmail.com>
2026-02-15 16:35:35 +0700

    feat: initial commit for frontend with vite and bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1>Music Community Frontend</h1>
+      <Button variant="primary">Kiểm tra Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
